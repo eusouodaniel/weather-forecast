6be3375887557de73acf1a64231920e7869822d1
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-07-12 11:18:26 -0300

    NEW: Search weather forecast

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [location, setLocation] = useState(false);
+	const [weather, setWeather] = useState(false);
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			getWeather(position.coords.latitude, position.coords.longitude);
+			setLocation(true);
+		})
+	})
+
+	let getWeather = async (lat, long) => {
+		let weather = await axios.get(process.env.REACT_APP_WEATHER_URL, {
+			params: {
+				lat: lat,
+				lon: long,
+				appid: process.env.REACT_APP_WEATHER_KEY,
+				lang: 'pt',
+				units: 'metric'
+			}
+		})
+		setWeather(weather.data);
+	}
+
+	if (location == false) {
+		return (
+			<Fragment>
+				Preciso que você permita que eu acesse a sua localização :)
+			</Fragment>
+		);
+	} else if (weather == false) {
+		return (
+			<Fragment>
+				Buscando o clima
+			</Fragment>
+		);
+	} else {
+		return (
+			<Fragment>
+				<h3>Clima baseado nas sua atual situação - { weather['weather'][0]['description'] }</h3>
+				<hr />
+				<ul>
+					<li>Temperatura atual: { weather['main']['temp'] }</li>
+					<li>Temperatura máxima: { weather['main']['temp_max'] }</li>
+					<li>Temperatura mínima: { weather['main']['temp_min'] }</li>
+				</ul>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
